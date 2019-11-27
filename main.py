02699c8f663b9c7a570b22556ec296819629a82a
bitstuffing <eduardo.higuero@hotmail.com>
2019-11-27 17:33:20 +0100

    added settins menu

--- main.py
+++ main.py
@@ -25,6 +25,7 @@ MENU_OPTION_MARGIN = 45  # Option margin (px)
 
 ABOUT = [
     'using library version {0}'.format(pygameMenu.__version__),
+    'opensource PyMenu to use opensource PyGame',
     'by: @{0} with love'.format("bit"),
     pygameMenu.locals.TEXT_NEWLINE,
     'contact: {0}'.format("bit@gameboyzero.es")
@@ -32,17 +33,13 @@ ABOUT = [
 
 class PyMenu():
 
-
     clock = None
     main_menu = None
     about_menu = None
+    settings_menu = None
     surface = None
 
-    def main_background(self):
-        self.surface.fill(COLOR_BACKGROUND)
-
-    def main(self):
-
+    def __init__(self):
         #init
         pygame.init()
 
@@ -51,8 +48,19 @@ class PyMenu():
         pygame.display.set_caption('Menu principal')
         self.clock = pygame.time.Clock()
 
+    def main(self):
         # Create menus
+        self.createAboutMenu()
+        self.createSettingsMenu()
+        self.createMainMenu() #last, instance because of
 
+        # mainloop
+        self.mainloop()
+
+    def main_background(self):
+        self.surface.fill(COLOR_BACKGROUND)
+
+    def createAboutMenu(self):
         #about menu (sample)
         self.about_menu = pygameMenu.TextMenu(self.surface,
             bgfun=self.main_background,
@@ -78,6 +86,7 @@ class PyMenu():
         self.about_menu.add_line(pygameMenu.locals.TEXT_NEWLINE)
         self.about_menu.add_option('Return to menu', pygameMenu.events.BACK)
 
+    def createMainMenu(self):
         #main menu
         self.main_menu = pygameMenu.Menu(self.surface,
             bgfun=self.main_background,
@@ -100,44 +109,62 @@ class PyMenu():
 
         self.main_menu.add_option('Repositorio', self.progressbar)
         self.main_menu.add_option('Tutorial', self.about_menu)
-        self.main_menu.add_option('Configuracion', self.about_menu)
+        self.main_menu.add_option('Configuracion', self.settings_menu)
         self.main_menu.add_option('Salir', pygameMenu.events.EXIT)
 
+    def mainloop(self):
         #limit fps
         self.main_menu.set_fps(FPS)
-
         #main loop
         exit = False
         self.main_menu.mainloop()
         while not exit:
-
             #it's just fps limitation
             self.clock.tick(FPS)
-
             #colored background
             self.main_background()
-
-            #events
+            #get events and configure
             events = pygame.event.get()
             print(str(events))
             for event in events:
                 if event.type == pygame.QUIT:
                     exit = True
                 elif event.type == pygame.KEYDOWN:
-                    print("keydown!")
                     if event.key == pygame.K_ESCAPE and self.main_menu.is_enabled():
                         exit = True
-
             # Main menu
             self.main_menu.mainloop(events, disable_loop=False)
-
             # Flip surface
             pygame.display.flip()
-
-        print("bye")
         exit()
 
+    def save_name(self,name):
+        print("%s saved!"%name)
 
+    def createSettingsMenu(self):
+
+        self.settings_menu = pygameMenu.Menu(self.surface,
+            bgfun=self.main_background,
+            color_selected=COLOR_BLUE,
+            font=pygameMenu.font.FONT_8BIT,
+            font_color=COLOR_LIGHT_GREEN,
+            font_size=30,
+            menu_alpha=70,
+            menu_color=MENU_BACKGROUND_COLOR,
+            menu_height=int(WINDOW_SIZE[1] * 0.85),
+            menu_width=int(WINDOW_SIZE[0] * 0.9),
+            onclose=pygameMenu.events.DISABLE_CLOSE,
+            option_shadow=False,
+            title='Configuraciones',
+            widget_alignment=pygameMenu.locals.ALIGN_LEFT,
+            option_margin=MENU_OPTION_MARGIN,
+            window_height=WINDOW_SIZE[1],
+            window_width=WINDOW_SIZE[0]
+        )
+        self.settings_menu.add_text_input('First name: ', default='Bit', onreturn=self.save_name)
+        self.settings_menu.add_option('Return to menu', pygameMenu.events.BACK)
+
+        print("done!")
 
     def progressbar(self):
         #hide main menu
@@ -192,7 +219,7 @@ class PyMenu():
 
             pygame.display.flip()
 
-        #self.main_menu.mainloop(events, disable_loop=False)
+        #show main menu
         self.main_menu.enable()
         self.main_menu.reset(1)
 
