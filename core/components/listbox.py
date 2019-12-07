08371fa1b337919a9ebbd2a7a07291e6d5579bd6
bitstuffing <eduardo.higuero@hotmail.com>
2019-12-07 20:14:41 +0100

    config list... (WIP)

--- core/components/listbox.py
+++ core/components/listbox.py
@@ -36,14 +36,17 @@ class ListBox():
         sizeX = self.width - (self.margin*2) - (self.padding*2) - self.barWidth
         sizeY = (self.height-(self.padding*(self.visibleOptions+1)) - (self.margin*2)) / self.visibleOptions
 
-        for i in range(0,self.visibleOptions):
+        selected = 3
+        first = 0
+        last = self.visibleOptions
+        for i in range(first,last):
             if self.centered:
                 x = ((WINDOW_SIZE[0])/2) - (sizeX/2)
             else:
                 x = self.x
 
             y = self.y + self.margin + ((i+1)*self.padding) + ((i)*sizeY)
-            self.displayOption(self.list[i], x, y, sizeX, sizeY)
+            self.displayOption(element=self.list[i], x=x, y=y, sizeX=sizeX, sizeY=sizeY,selected_field=bool(i==selected),selected_choice=0)
 
         #display lateral bar
         self.displayBar(sizeX,sizeY)
@@ -61,12 +64,18 @@ class ListBox():
         pygame.draw.rect(self.surface, COLOR_GRAY, button_rect, 0)
 
 
-    def displayOption(self,element,x,y,sizeX,sizeY):
+    def displayOption(self,element,x,y,sizeX,sizeY,selected_field=False,selected_choice=0,selected_margin=15):
 
         text = element["title"]
+        list = element["choice"]
         #fill title
         button_rect = pygame.Rect(x, y, (sizeX/3)-self.padding, sizeY)
         pygame.draw.rect(self.surface, COLOR_BLACK, button_rect, 0)
+
+        if selected_field:
+            #draw background rectangle
+            button_rect_background = pygame.Rect(x+selected_margin, y+selected_margin, (sizeX/3)-self.padding-selected_margin*2, sizeY-selected_margin*2)
+            pygame.draw.rect(self.surface, COLOR_BLUE, button_rect_background, 0)
 
         xT = x+self.padding
         yT = y+sizeY/2-(self.font.size(text)[1]/2)
@@ -74,5 +83,19 @@ class ListBox():
         self.surface.blit(txt, (xT, yT))
 
         #fill options
-        button_rect = pygame.Rect(x+(sizeX/3)-self.padding, y, (sizeX*2/3)-self.padding, sizeY)
+        firstX = x+(sizeX/3)-self.padding
+        lastX = (sizeX*2/3)-self.padding
+        button_rect = pygame.Rect(firstX, y, lastX, sizeY)
         pygame.draw.rect(self.surface, COLOR_DARK_GRAY, button_rect, 0)
+
+        barHeight = 20 #TODO
+        left_rect = pygame.Rect(firstX+self.padding, y+self.padding, barHeight, sizeY-(self.padding*2))
+        pygame.draw.rect(self.surface, COLOR_GRAY, left_rect, 0)
+        right_rect = pygame.Rect(firstX+lastX-(self.padding*2), y+self.padding, barHeight, sizeY-(self.padding*2))
+        pygame.draw.rect(self.surface, COLOR_GRAY, right_rect, 0)
+
+        text = list[selected_choice]
+        xT = x + (sizeX*2/3)-(self.font.size(text)[0]/2)
+        yT = y+sizeY/2-(self.font.size(text)[1]/2)
+        txt = self.font.render(text, True, COLOR_WHITE)
+        self.surface.blit(txt, (xT, yT))
