4728d9c82fd0a9d24ee2081c22d30d006e50a7ad
bitstuffing <eduardo.higuero@hotmail.com>
2019-12-30 16:29:19 +0100

    issue #3 time in bar like a widget

--- core/components/upbar.py
+++ core/components/upbar.py
@@ -4,7 +4,7 @@ import os
 from core.colors import *
 from core.utils import Utils
 from core.components.menu import Menu
-
+from datetime import datetime
 from core.constants import *
 
 class UpBar():
@@ -14,13 +14,43 @@ class UpBar():
         self.bar = pygame.Surface((WINDOW_SIZE[0], BARSIZE), pygame.SRCALPHA)
         self.font = pygame.font.Font(os.path.join(PATH,"assert/fonts","DejaVuSans.ttf"), FONT_SIZE)
         self.menu = Menu(title="Menu", first=(0,0) ,parent=(WINDOW_SIZE[0],BARSIZE), font=self.font, surface=self.surface)
+        self.margin = 5
 
     def draw(self):
         self.drawBackground()
+
+        self.drawWidgets()
+
+        self.refresh()
+
         self.menu.draw()
 
     def drawBackground(self):
         # add alpha to tuple {transform (,,,) to (,,,,ALPHA)}
         black_with_alpha = COLOR_BLACK + (ALPHA, )
         pygame.draw.rect(self.bar, black_with_alpha, (0,0, WINDOW_SIZE[0],BARSIZE))
-        self.surface.blit(self.bar,(0,0))
+
+    def refresh(self):
+        self.surface.blit(self.bar, (0, 0))
+
+    def drawWidgets(self):
+        width = self.drawTime()
+
+
+    def drawTime(self):
+
+        text = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+        width = self.font.size(text)[0] + (self.margin * 2)
+        height = self.font.size(text)[1] + (self.margin * 2)
+
+        white_with_alpha = COLOR_WHITE + (ALPHA,)
+        pygame.draw.rect(self.bar, white_with_alpha, (WINDOW_SIZE[0]-width, 0, width, BARSIZE))
+
+        txt = self.font.render(text, True, COLOR_BLACK)
+
+        x = WINDOW_SIZE[0]-width+self.margin
+        y = height / 2
+        textPoint = (x, y)
+        self.surface.blit(txt, textPoint)
+
+        return width
