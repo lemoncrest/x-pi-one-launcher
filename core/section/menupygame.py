5eba219741fd394daa3e87f858882e231eafc78c
bitstuffing <eduardo.higuero@hotmail.com>
2020-01-06 18:43:29 +0100

    quit menu section organized

--- core/section/menupygame.py
+++ core/section/menupygame.py
@@ -2,15 +2,9 @@
 
 # python2 issues, div with float, not int
 from __future__ import division
-import gettext
 import pygame
-import json
-import random
 import os
-import codecs
-import sys
-import subprocess
-import time
+
 try:
     from datetime import datetime, timedelta
 except:
@@ -34,9 +28,9 @@ from core.section.gogpygame import GOGPygame
 from core.section.itchpygame import ItchPygame
 from core.section.repositorypygame import RepositoryPygame
 from core.section.settingspygame import SettingsPygame
-from core.components.floatlist import FloatList
+from core.section.quitpygame import QuitPygame
 
-class MenuPygame(MainPyGame, SquaredMenu, SimpleMenu, DownloadProgressBar, GOGPygame, ItchPygame, RepositoryPygame, SettingsPygame):
+class MenuPygame(MainPyGame, SquaredMenu, SimpleMenu, DownloadProgressBar, GOGPygame, ItchPygame, RepositoryPygame, SettingsPygame, QuitPygame):
 
     def __init__(self):
         # init
@@ -333,68 +327,6 @@ class MenuPygame(MainPyGame, SquaredMenu, SimpleMenu, DownloadProgressBar, GOGPy
 
     def drawComponents(self):
         self.upbar.draw()
-
-    def halt(self):
-        pass
-
-    def reboot(self):
-        pass
-
-    def exitAndAsk(self):
-        options = [
-            {
-                "title": "Cerrar launcher",
-                "action": quit
-            }
-        ]
-        if os.path.exists("/usr/bin/kodi"):
-            options.insert(0,{
-                "title": "Kodi",
-                "action": self.launchKodi
-            })
-        if os.path.exists("/usr/bin/emulationstation"):
-            options.insert(0,{
-                "title": "EmulationStation",
-                "action": self.launchEmulationstation
-            })
-        if os.path.exists("/usr/bin/retroarch"):
-            options.insert(0,{
-                "title": "RetroArch",
-                "action": self.launchRetroarch
-            })
-        #clear last menu
-        self.main_background()
-        floatList = FloatList(surface=self.surface, clock=self.clock, options=options)
-        floatList.draw()
-
-    def launchRetroarch(self):
-        os.system("/usr/bin/retroarch &")
-        # quit()
-
-    def launchKodi(self):
-        os.system("/usr/bin/kodi &")
-        # quit()
-
-    def launchEmulationstation(self):
-        os.system("/usr/bin/emulationstation &")
-        #quit()
-
-
-    def quit(self):
-        options = [
-            {
-                "title" : "Apagar el sistema",
-                "action": self.halt
-            },{
-                "title" : "Reiniciar",
-                "action" : self.reboot
-            },{
-                "title" : "Salir de la consola",
-                "action" : self.exitAndAsk
-            }
-        ]
-        floatList = FloatList(surface=self.surface,clock=self.clock,options=options)
-        floatList.draw()
 
     def main_background(self):
 
---
+++ core/section/quitpygame.py
@@ -0,0 +1,67 @@
+import os
+
+from core.components.floatlist import FloatList
+
+class QuitPygame():
+
+    def halt(self):
+        pass
+
+    def reboot(self):
+        pass
+
+    def exitAndAsk(self):
+        options = [
+            {
+                "title": "Cerrar launcher",
+                "action": quit
+            }
+        ]
+        if os.path.exists("/usr/bin/kodi"):
+            options.insert(0,{
+                "title": "Kodi",
+                "action": self.launchKodi
+            })
+        if os.path.exists("/usr/bin/emulationstation"):
+            options.insert(0,{
+                "title": "EmulationStation",
+                "action": self.launchEmulationstation
+            })
+        if os.path.exists("/usr/bin/retroarch"):
+            options.insert(0,{
+                "title": "RetroArch",
+                "action": self.launchRetroarch
+            })
+        #clear last menu
+        self.main_background()
+        floatList = FloatList(surface=self.surface, clock=self.clock, options=options)
+        floatList.draw()
+
+    def launchRetroarch(self):
+        os.system("/usr/bin/retroarch &")
+        # quit()
+
+    def launchKodi(self):
+        os.system("/usr/bin/kodi &")
+        # quit()
+
+    def launchEmulationstation(self):
+        os.system("/usr/bin/emulationstation &")
+        #quit()
+
+
+    def quit(self):
+        options = [
+            {
+                "title" : "Apagar el sistema",
+                "action": self.halt
+            },{
+                "title" : "Reiniciar",
+                "action" : self.reboot
+            },{
+                "title" : "Salir de la consola",
+                "action" : self.exitAndAsk
+            }
+        ]
+        floatList = FloatList(surface=self.surface,clock=self.clock,options=options)
+        floatList.draw()
