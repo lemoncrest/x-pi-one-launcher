95fd1711ceb8ec7a2432599e415d7c9d94f04d11
bitstuffing <eduardo.higuero@hotmail.com>
2020-01-17 14:43:22 +0100

    (WIP) wifi configuration menu

--- core/section/wificonfigurationpygame.py
+++ core/section/wificonfigurationpygame.py
@@ -3,6 +3,8 @@ import pygame
 import os
 import json
 
+import subprocess
+
 from core.constants import *
 
 from core.component.progressbar import ProgressBar
@@ -23,17 +25,38 @@ class WifiConfigurationPygame():
 
         networks = []
 
-        #fill with network list
+        #cmd = "awk 'NR==3 {print $1}''' /proc/net/wireless" # needs a out.replace(":","")
+        cmd = "cat /proc/net/wireless | perl -ne '/(\w+):/ && print $1'"
 
+        #proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+        #(out, err) = proc.communicate()
+        #interface = out.decode("utf-8")
+
+        #fill with network list
+        #cmd = 'iwlist wlan0 scan | grep ESSID'
+        cmd = "for i in $(ls /sys/class/net/ | egrep -v ^lo$); do sudo iw dev $i scan | grep SSID | awk '{print substr($0, index($0,$2)) }'; done 2>/dev/null | sort -u"
+
+        p = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
+
+        (out, err) = p.communicate()
+
+        p_status = p.wait() # This makes the wait possible
+
+        lists = out.decode("utf-8")
+
+        logger.debug(lists)
+
+        for list in lists.split("\n"):
+            logger.debug(list)
 
         x = int(WINDOW_SIZE[0]) - 100
-        y = 100
+        y = 50
 
         margin = 0
 
         self.listbox = BoxList(
             width=int(WINDOW_SIZE[0])-200,
-            height=int(WINDOW_SIZE[1])-200,
+            height=int(WINDOW_SIZE[1])-100,
             x=x,
             y=y,
             margin=margin,
